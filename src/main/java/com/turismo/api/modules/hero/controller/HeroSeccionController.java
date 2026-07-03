a96ba385419fae22b64c7048225e29deb6ad0744
saulcusi-GT <saulcusipumamarino@gmail.com>
2026-07-03 07:41:32 -0400

    Corregir rutas duplicadas en Swagger

--- src/main/java/com/turismo/api/modules/hero/controller/HeroSeccionController.java
+++ src/main/java/com/turismo/api/modules/hero/controller/HeroSeccionController.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping({"/api/hero", "/hero"})
+@RequestMapping("/api/hero")
 @RequiredArgsConstructor
 public class HeroSeccionController {
 
